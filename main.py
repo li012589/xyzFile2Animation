10c28c9c5dc542fa870004d15d18953d4443a159
Li SH <li012589li@outlook.com>
2019-06-20 16:18:27 +0800

    add npz support

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ import gr
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-xyz",default=None,help="name of the xyz file")
+parser.add_argument("-filename",default=None,help="name of the data file")
 parser.add_argument("-output",default=None,help="name of the output files")
-parser.add_argument("-batch",default=100,type=int,help="number of xyz to show")
+parser.add_argument("-batch",default=100,type=int,help="number of time steps to show")
 parser.add_argument("-bond",default=1.2,type=float,help="mogli's bong_param")
+parser.add_argument("-name",default=None,help = 'name of the data saved in npz file')
+parser.add_argument("-smile",default=None,help = "Smile expression of the molecule")
+parser.add_argument("-scaling",default=10,type=float,help = "scaling factor of npz data, default is for nm to ångströms")
+parser.add_argument("-fixx",default=0,type=float,help="fix x axis")
+parser.add_argument("-fixy",default=23.222,type=float,help="fix y axis")
+parser.add_argument("-fixz",default=0,type=float,help="fix z axis")
 args = parser.parse_args()
 
-molecules = mogli.read(args.xyz)
+if args.filename.endswith('.xyz'):
+    molecules = mogli.read(args.filename)
+elif args.filename.endswith('.npz'):
+    fixs = np.array([args.fixx,args.fixy,args.fixz])
+    molecules = mogli.load(args.filename,args.name,args.smile,args.scaling,fixs)
 
 for t in range(args.batch):
 
--- readme.md
+++ readme.md
@@ -9,3 +9,11 @@ python ./main.py -xyz "./examples/alanine-dipeptide-3x250ns-heavy-atom-positions
 ```
 
 Note that to create a `mov` file, run this before the above line in your terminal: `export GKS_WSTYPE=mov`.
+
+Or, using a `npz` file, not that to use this you have to use my tweaked version of  [mogli](https://github.com/li012589/mogli).
+
+```bash
+python ./main.py -filename "./examples/alanine-dipeptide-3x250ns-heavy-atom-positions.npz" -batch 200
+-name arr_0 -smile "CC(=O)NC(C)C(=O)NC" -scaling 10 -fixy 23.222
+```
+

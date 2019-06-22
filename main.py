089a592e2983a939de8a52882435926ccac8c34b
Li SH <li012589li@outlook.com>
2019-06-22 17:17:37 +0800

    add fix bond option

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ parser.add_argument("-scaling",default=10,type=float,help = "scaling factor of n
 parser.add_argument("-fixx",default=0,type=float,help="fix x axis")
 parser.add_argument("-fixy",default=23.222,type=float,help="fix y axis")
 parser.add_argument("-fixz",default=0,type=float,help="fix z axis")
+parser.add_argument("-fix_bond",action="store_true",help="fix bond according to SMILES")
+
 args = parser.parse_args()
 
 if args.output is not None:
@@ -29,13 +31,20 @@ elif args.filename.endswith('.npz'):
     fixs = np.array([args.fixx,args.fixy,args.fixz])
     molecules = mogli.load(args.filename,args.name,args.smile,args.scaling,fixs)
 
+    if args.fix_bond:
+        bonds_method = 'fix_bond'
+        bonds_param = args.smile
+    else:
+        bonds_method = 'radii'
+        bonds_param = args.bond
+
+
 for t in range(args.batch):
 
     gr.clearws()
     gr.setviewport(0, 0.7, 0, 0.7)
     gr.setwindow(0.1, 0.9, 0.05, 0.85)
-    mogli.draw(molecules[t], bonds_param=args.bond, camera=((10, 0, 10),
-                                                       (0, 0, 0),
-                                                       (0, 1, 0)))
+    #mogli.draw(molecules[t], bonds_param=args.bond, camera=((12, 0, 12),(0, 0, 0),(0, 1, 0)))
+    mogli.draw(molecules[t], bonds_method=bonds_method,bonds_param=bonds_param, camera=((12, 0, 12),(0, 0, 0),(0, 1, 0)))
 
     gr.updatews()
